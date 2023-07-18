adedbc5dfd61d3c87ad27cdc9ea2910d1fcf07a8
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-07-18 12:31:10 +0200

    add host with nanostore

--- apps/shell/src/index.tsx
+++ apps/shell/src/index.tsx
@@ -1,6 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from './app';
-
-ReactDOM.render(<App name="Gambonny" />, document.getElementById('root'));
+import('./bootstrap')
--- apps/shell/webpack.config.js
+++ apps/shell/webpack.config.js
@@ -1,18 +1,53 @@
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+let plugins = [];
+
+plugins.push(
+  new HtmlWebpackPlugin({
+    template: path.join(__dirname, "public", "index.html"),
+  })
+)
+
+plugins.push(
+  new ModuleFederationPlugin({
+    name: "host",
+    remotes: {
+      host: "host@http://localhost:3000/remoteEntry.js",
+    },
+    exposes: {
+      "./counter": "./src/stores/counter.ts",
+    },
+    shared: {
+      react: {
+        singleton: true,
+        requiredVersion: "^17.0.2",
+      },
+      "react-dom": {
+        singleton: true,
+        requiredVersion: "^17.0.2",
+      },
+    },
+  }),
+)
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new BundleAnalyzerPlugin());
+}
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.tsx',
   devtool: 'inline-source-map',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
   },
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
+    port: 3000,
   },
   module: {
     rules: [
@@ -31,28 +66,5 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  plugins:[
-    new ModuleFederationPlugin({
-      name: "host",
-      remotes: {
-        host: "host@http://localhost:3000/remoteEntry.js",
-      },
-      exposes: {
-        "./counter": "./src/stores/counter.ts",
-      },
-      shared: {
-        react: {
-          singleton: true,
-          requiredVersion: "^18.2.0",
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: "^18.2.0",
-        },
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    })
-  ]
+  plugins: plugins,
 }
