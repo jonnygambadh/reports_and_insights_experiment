513649db3ee40b20919dab96d7d81ca6b014fcb7
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-07-18 12:04:48 +0200

    add nanostores and module federation host

--- apps/shell/webpack.config.js
+++ apps/shell/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.jsx',
+  entry: './src/index.tsx',
   devtool: 'inline-source-map',
   output: {
     path: path.join(__dirname, '/dist'),
@@ -20,12 +21,36 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ]
   },
   resolve: {
-    extensions: ['.jsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins:[
+    new ModuleFederationPlugin({
+      name: "host",
+      remotes: {
+        host: "host@http://localhost:3000/remoteEntry.js",
+      },
+      exposes: {
+        "./counter": "./src/stores/counter.ts",
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: "^18.2.0",
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: "^18.2.0",
+        },
+      },
+    }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
     })
